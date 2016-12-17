ace8d6eb87b30dcfb0be56f2a340e321d32d6d40
Eliran Haziza <eliranhaze@gmail.com>
2016-12-17 12:20:48 +0200

    minify feed fix

--- utils/minify.py
+++ utils/minify.py
@@ -1,21 +1,11 @@
 import re
 
 def minify_feed(content):
-    content = re.sub('<content:encoded[\s\S]*?</content:encoded>', '', content)
-    content = re.sub('<description[\s\S]*?</description>', '', content)
-    return content
-
-def minify_html2(content):
-    content = re.sub('<footer[\s\S]*?</footer>', '', content)
-    content = re.sub('<nav[\s\S]*?</nav>', '', content)
-    content = re.sub('<script[\s\S]*?</script>', '', content)
-    content = re.sub('<form[\s\S]*?</form>', '', content)
-    content = re.sub('<style[\s\S]*?</style>', '', content)
-    content = re.sub('<h[1-6][\s\S]*?</h[1-6]>', '', content)
-    content = re.sub('<!--[\s\S]*?-->', '', content)
-    content = re.sub('href=".*?"', '', content)
-    content = re.sub('src=".*?"', '', content)
-    content = ' '.join(content.split())
+    content = content.replace('<description/>','').replace('<media:description/>','').replace('<content:encoded/>','').replace('<category/>','')
+    content = re.sub('<description[ >][\s\S]*?</description>', '', content)
+    content = re.sub('<media:description[ >][\s\S]*?</media:description>', '', content)
+    content = re.sub('<content:encoded[ >][\s\S]*?</content:encoded>', '', content)
+    content = re.sub('<category[ >][\s\S]*?</category>', '', content)
     return content
 
 def minify_html(content):
@@ -26,8 +16,8 @@ def minify_html(content):
     content = re.sub('<style[\s\S]*?</style>', '', content)
     content = re.sub('<h[1-6][\s\S]*?</h[1-6]>', '', content)
     content = re.sub('<!--[\s\S]*?-->', '', content)
-    content = re.sub('href=".*?"', '', content)
-    content = re.sub('src=".*?"', '', content)
+    content = re.sub('href=".*?"', ' ', content)
+    content = re.sub('src=".*?"', ' ', content)
     content = content.replace('  ','').replace('\n\n','\n')
     return content
 
