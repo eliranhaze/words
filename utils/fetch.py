d8315fd9606de7595e437ac9106415e3df7ae831
Eliran Haziza <eliranhaze@gmail.com>
2016-12-30 14:50:09 +0200

    disable requests warnings

--- utils/fetch.py
+++ utils/fetch.py
@@ -4,6 +4,8 @@ import urlparse
 
 from executor import Executor
 
+requests.packages.urllib3.disable_warnings()
+
 session = requests.Session()
 session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
 
