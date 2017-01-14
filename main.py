925ed66c945a782acd1feea34ce2888e9ba122e1
Eliran Haziza <eliranhaze@gmail.com>
2017-01-14 12:26:47 +0200

    min len threshold change

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def make_title(title):
 def fetch_articles(sources, from_date=None):
     result = []
     erred = []
-    min_text_len = 2000
+    min_text_len = 4000
     for source in sources:
         urls = set()
         titles = set()
