63cdb74b4118eb3759dd0af47fffab3e88c6acc5
Eliran Haziza <eliranhaze@gmail.com>
2017-02-04 12:59:22 +0200

    input option in count

--- count.py
+++ count.py
@@ -16,11 +16,12 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', dest='url')
     parser.add_argument('--file', dest='file')
+    parser.add_argument('--input', dest='input')
     parser.add_argument('--out', dest='out')
     parser.add_argument('--list', dest='list_words', action='store_true')
     args = parser.parse_args()
-    if not args.file and not args.url:
-        print 'either --url or --file is required'
+    if not args.file and not args.url and not args.input:
+        print 'one of --url, --file, --input is required'
         exit()
     return args
 
@@ -33,9 +34,14 @@ def main():
     if args.url:
         print 'fetching text from', args.url
         text = textify_web_content(fetcher.fetch(args.url, verify=False).content)
-    else:
+    elif args.file:
         print 'reading file', args.file
         text = open(args.file).read()
+    elif args.input:
+        print 'reading input'
+        text = args.input
+    else:
+        raise ValueError('no args')
 
     if args.out:
         print 'with output'
