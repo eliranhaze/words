28835645d676b9b08712c124c5c7ad2d298ca537
Eliran Haziza <eliran@cogitosys.com>
2017-08-11 16:19:07 +0300

    data update

--- brains/data.py
+++ brains/data.py
@@ -73,6 +73,7 @@ DATA_SOURCES = {
          'http://www.nybooks.com/articles/2016/09/29/hobbes-spinoza-locke-leibniz-hume-wrestled-new/',
          'http://www.nybooks.com/daily/2017/07/01/iraq-the-battle-to-come/',
          'http://www.nybooks.com/articles/2017/06/22/israels-irrational-rationality/',
+         'http://www.nybooks.com/articles/2017/08/17/henry-david-thoreau-true-american/',
 
          # ny times
          'https://www.nytimes.com/2017/02/24/style/modern-love-when-your-greatest-romance-is-friendship.html',
