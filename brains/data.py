fb8e3059a46e03fa3bb7f88f2887082e890ec1dc
Eliran Haziza <eliranhaze@gmail.com>
2017-08-18 15:04:58 +0300

    data update

--- brains/data.py
+++ brains/data.py
@@ -77,6 +77,7 @@ DATA_SOURCES = {
          'http://www.nybooks.com/articles/2017/06/22/israels-irrational-rationality/',
          'http://www.nybooks.com/articles/2017/08/17/henry-david-thoreau-true-american/',
          'http://www.nybooks.com/articles/2013/02/21/speak-memory/',
+         'http://www.nybooks.com/articles/2001/04/12/southern-comfort/',
 
          # ny times
          'https://www.nytimes.com/2017/02/24/style/modern-love-when-your-greatest-romance-is-friendship.html',
