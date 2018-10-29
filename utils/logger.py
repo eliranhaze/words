47aa03398bf0c4721f099c9991b9844298df35fd
Eliran Haziza <eliranhaze@gmail.com>
2018-10-28 20:43:21 -0400

    changed log file to generic

--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,7 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 
 formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
-handler = TimedRotatingFileHandler('log/words.log', when='midnight', backupCount=10) 
+handler = TimedRotatingFileHandler('log/app.log', when='midnight', backupCount=10) 
 handler.setFormatter(formatter)
 
 def get_logger(name):
