dddd06f142ca22c27817621c3ed917ffbd7f7b8b
Eliran Haziza <eliranhaze@gmail.com>
2018-10-28 20:48:42 -0400

    added pkl util

---
+++ utils/pkl.py
@@ -0,0 +1,23 @@
+import cPickle as cpkl
+import os
+
+from logger import get_logger
+logger = get_logger('pkl')
+
+def save(data, name):
+    filename = _format_filename(name)
+    logger.debug('saving data to %s' % filename)
+    cpkl.dump(data, open(filename, 'wb'))
+    logger.debug('saved data to %s' % filename)
+
+def load(name):
+    filename = _format_filename(name)
+    if os.path.exists(filename):
+        logger.debug('loading data from %s' % filename)
+        data = cpkl.load(open(filename, 'rb'))
+        logger.debug('loaded data from %s' % filename)
+        return data
+    logger.debug('%s does not exist')
+
+def _format_filename(name):
+    return '.%s.pkl' % name
